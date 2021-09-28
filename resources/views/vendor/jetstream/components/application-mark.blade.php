43fdec177e72858b5131d880b04f25a13adc5e3e
Travis Saylor <travis.saylor@gmail.com>
2021-09-28 18:39:42 -0500

    Update logo

--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,4 +1,75 @@
-<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-  <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
-  <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
+<svg xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://www.w3.org/2000/svg"
+    xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:svg="http://www.w3.org/2000/svg"
+    xmlns:ns1="http://sozi.baierouge.fr" xmlns:xlink="http://www.w3.org/1999/xlink" id="svg2472"
+    sodipodi:docname="aiga_bar_.svg" inkscape:export-filename="/home/joanna/gfx/vector/anon/aiga/aiga_bar_.png"
+    viewBox="0 0 362.48 363.27" sodipodi:version="0.32" inkscape:export-xdpi="49.657364"
+    inkscape:output_extension="org.inkscape.output.svg.inkscape" inkscape:export-ydpi="49.657364"
+    inkscape:version="0.46" sodipodi:docbase="/datas/Projs/Cliparts Stocker/tmp/AIGA"
+    {{ $attributes }}
+    >
+    <sodipodi:namedview id="base" bordercolor="#666666" inkscape:pageshadow="2" inkscape:window-width="641"
+        pagecolor="#ffffff" inkscape:zoom="0.43415836" inkscape:window-x="0" showgrid="false" borderopacity="1.0"
+        inkscape:current-layer="svg2472" inkscape:cx="305.25953" inkscape:cy="417.84947" inkscape:window-y="0"
+        inkscape:window-height="715" inkscape:pageopacity="0.0" />
+    <g id="g2482">
+        <path id="path2484" style="fill:#000000"
+            d="m0 0h362.48l-157.52 194.57v124.61h87.2c29.25 0 29.5 44.09 0 44.09h-217.5c-29.504 0-29.504-44.09-0.004-44.09h85.714v-125.57l-160.37-193.61z" />
+        <g id="g2486">
+            <path id="path2488" style="fill:#ffffff"
+                d="m210.16 137.3c15.53 0 28.12-12.59 28.12-28.13 0-15.528-12.59-28.123-28.12-28.123-15.54 0-28.13 12.595-28.13 28.123 0 15.54 12.59 28.13 28.13 28.13z" />
+            <path id="path2490" style="fill:#ffffff" d="m210.16 109.17" />
+        </g>
+    </g>
+    <metadata>
+        <rdf:RDF>
+            <cc:Work>
+                <dc:format>image/svg+xml</dc:format>
+                <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+                <cc:license rdf:resource="http://creativecommons.org/licenses/publicdomain/" />
+                <dc:publisher>
+                    <cc:Agent rdf:about="http://openclipart.org/">
+                        <dc:title>Openclipart</dc:title>
+                    </cc:Agent>
+                </dc:publisher>
+                <dc:title>aiga bar</dc:title>
+                <dc:date>2009-05-02T19:56:40</dc:date>
+                <dc:description>Set of international airport symbols.
+                    \n
+                    \nSource: http://www.aiga.org/content.cfm/symbol-signs
+                    \n
+                    \nConverted to SVG by Jean-Victor Balin.</dc:description>
+                <dc:source>https://openclipart.org/detail/25727/aiga-bar-by-anonymous-25727</dc:source>
+                <dc:creator>
+                    <cc:Agent>
+                        <dc:title>Anonymous</dc:title>
+                    </cc:Agent>
+                </dc:creator>
+                <dc:subject>
+                    <rdf:Bag>
+                        <rdf:li>aiga</rdf:li>
+                        <rdf:li>aiga no bg</rdf:li>
+                        <rdf:li>alcohol</rdf:li>
+                        <rdf:li>bar</rdf:li>
+                        <rdf:li>black and white</rdf:li>
+                        <rdf:li>drink</rdf:li>
+                        <rdf:li>externalsource</rdf:li>
+                        <rdf:li>glass</rdf:li>
+                        <rdf:li>map symbol</rdf:li>
+                        <rdf:li>martini</rdf:li>
+                        <rdf:li>sign</rdf:li>
+                        <rdf:li>silhouette</rdf:li>
+                        <rdf:li>symbol</rdf:li>
+                    </rdf:Bag>
+                </dc:subject>
+            </cc:Work>
+            <cc:License rdf:about="http://creativecommons.org/licenses/publicdomain/">
+                <cc:permits rdf:resource="http://creativecommons.org/ns#Reproduction" />
+                <cc:permits rdf:resource="http://creativecommons.org/ns#Distribution" />
+                <cc:permits rdf:resource="http://creativecommons.org/ns#DerivativeWorks" />
+            </cc:License>
+        </rdf:RDF>
+    </metadata>
 </svg>
+
